5f76d239d875aebc84629f90b6e486ad9b0b294c
benpapac <ben.papac@gmail.com>
2021-09-04 10:53:26 -0400

    Added alts.js

---
+++ alts.js
@@ -0,0 +1,33 @@
+
+const minions = [
+	'bob',
+	'kevin',
+	'stuart',
+	'dave',
+	'jerry',
+	'kevin',
+	'mark',
+	'tim',
+	'phil',
+	'carl'
+];
+
+// to make the .map change without creating a callable function: 
+const capitalizedMinions =
+minions.map(element =>
+    {
+if(element === 'kevin')
+{return element}
+return element[0].toUpperCase() + element.substring(1)
+    }
+    );
+
+/// NOTICE the SLICE method -- it has more utility than .substring()
+    let capatalizedMinions = minions.map((element)=> {
+    if (element === 'kevin'){
+      return element;
+    } 
+        return element.charAt(0).toUpperCase() + element.slice(1);
+    
+});
+console.log(capatalizedMinions);
--- practice.js
+++ practice.js
@@ -11,16 +11,27 @@ const minions = [
 	'carl'
 ];
 
-const prices = [
-    '$2.00','$33.33','$0.89'
-]
+
 
 
 listMinions(minions);
+const cappedNames = capNames(minions);
+console.log(cappedNames);
 
 function listMinions(array) {
     array.forEach(element => {
         console.log(element);
         
     });
+}
+
+function capNames(array){
+
+   let newArray=  array.map(element=>{
+        if (element === 'kevin') {return element};
+        let newName = element[0].toUpperCase() + element.substring(1);
+		return newName;
+        });
+
+	return newArray;
 }
