aca8e734bbe4a70fefeb316d100696524c094016
benpapac <ben.papac@gmail.com>
2021-09-04 12:32:22 -0400

    Added Joy's alt for find and replace Kevin

--- alts.js
+++ alts.js
@@ -34,5 +34,20 @@ return element[0].toUpperCase() + element.substring(1)
 });
 console.log(capatalizedMinions);
 
+/// CAP first kevin and REMOVE kevin/// --- Joy's version 
 
-
+function KevIndex() {
+   const p =
+    capitalizedMinions.findIndex(
+    (minion)=>
+    {
+     return  minion === uncapitalizedMinion
+    }
+);
+return p;
+}
+function capKevin(capitalizedMins)
+{
+capitalizedMins.splice(KevIndex(),1, uncapitalizedMinion[0].toUpperCase() + uncapitalizedMinion.slice(1))
+capitalizedMins.splice(KevIndex(), 1);
+}
